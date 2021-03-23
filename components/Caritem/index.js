2a1dad5ca808d36b171e8115eda21b488aac9a81
Himanshu Purohit <himanshu1203@gmail.com>
2021-03-23 16:37:29 +0530

    Car Item Component

--- components/Caritem/index.js
+++ components/Caritem/index.js
@@ -4,15 +4,22 @@ import StyledButton from '../StyledButton';
 import styles from './styles';
 
 const Caritem = (props) => {
+
+    const {name, tagline, taglineCTA, image} = props;
     return (
         <View style={styles.carContainer}>
-        <ImageBackground source={require('../../assets/images/ModelS.jpeg')} style={styles.image}/>
+        <ImageBackground source={image} style={styles.image}/>
         <View style={styles.titles}>
-          <Text style={styles.title}>Model S</Text>
-          <Text style={styles.subtitle}>Starting at $69,420</Text>
+          <Text style={styles.title}>{name}</Text>
+          <Text style={styles.subtitle}>
+            {tagline}{' '}
+            <Text style={styles.taglineCTA}>
+              {taglineCTA}
+            </Text>
+          </Text>
         </View>
 
-        <View >
+        <View style={styles.buttonContainer}>
           <StyledButton 
             type="primary" 
             content="Custom Order" 
--- components/Caritem/styles.js
+++ components/Caritem/styles.js
@@ -24,6 +24,14 @@ export default styles = StyleSheet.create({
       height:'100%',
       resizeMode:'cover',
       position:"absolute"
+    },
+    buttonContainer:{
+      position:"absolute",
+      bottom:50,
+      width:'100%'
+    },
+    taglineCTA:{
+      textDecorationLine:"underline",
     }
   
   });
